37f0406656c26f2f140ea2e2517780e99dbfef3c
Shotiko Topchishvili <shotikotopchishvili0@gmail.com>
2020-06-03 01:55:15 +0400

    Updates

---
+++ precache-manifest.c262c5c55f59bb0141549b1c7010f86a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a30a66000f741c0c4de5f4068bc093a",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "bf186c7edef9919efa9d",
+    "url": "/weather/static/css/main.2cd03678.chunk.css"
+  },
+  {
+    "revision": "7290343f8f7a367bbb04",
+    "url": "/weather/static/js/2.c6ace58a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather/static/js/2.c6ace58a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf186c7edef9919efa9d",
+    "url": "/weather/static/js/main.7cce8760.chunk.js"
+  },
+  {
+    "revision": "bdd4e2bf0ce8946d3851",
+    "url": "/weather/static/js/runtime-main.51874b67.js"
+  },
+  {
+    "revision": "39ac499c2a7856b24ffe3ffde4c8cc61",
+    "url": "/weather/static/media/15.39ac499c.png"
+  },
+  {
+    "revision": "0cf9cad1347244da8a9e197998e0eff9",
+    "url": "/weather/static/media/16.0cf9cad1.png"
+  },
+  {
+    "revision": "3c85da3e400a073c51b3ea73bdf5a130",
+    "url": "/weather/static/media/17.3c85da3e.png"
+  },
+  {
+    "revision": "421c13f1b1eb0ae947bb64472fec6baa",
+    "url": "/weather/static/media/41.421c13f1.png"
+  },
+  {
+    "revision": "7342b70ba3beeb372040de2daa7165ec",
+    "url": "/weather/static/media/bg.7342b70b.jpg"
+  },
+  {
+    "revision": "7c8bf47d63179df0bc0ac93c2fb89850",
+    "url": "/weather/static/media/mob.7c8bf47d.jpg"
+  }
+]);
