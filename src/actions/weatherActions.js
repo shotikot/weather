62c0a6d60b4d46e0b5a8cbacf628f90a96bbff4d
Shotiko Topchishvili <shotikotopchishvili0@gmail.com>
2020-06-03 02:09:22 +0400

    Fixed mixed content issue

--- src/actions/weatherActions.js
+++ src/actions/weatherActions.js
@@ -2,7 +2,7 @@ import {
   GET_LOCATION,
   GET_CURRENT_CITY_FORECAST,
   CHANGE_CITY,
-  GET_HOURLY
+  GET_HOURLY,
 } from "./types";
 
 export const getLocation = () => dispatch => {
@@ -10,7 +10,7 @@ export const getLocation = () => dispatch => {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
     fetch(
-      `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=fB40fnXIHKyL5EH90VyfZ8YbIQeHtv61&q=${lat}%2C${lon}`
+      `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=fB40fnXIHKyL5EH90VyfZ8YbIQeHtv61&q=${lat}%2C${lon}`
     )
       .then(response => response.json())
       .then(json => {
@@ -18,8 +18,8 @@ export const getLocation = () => dispatch => {
           type: GET_LOCATION,
           payload: {
             key: json.Key,
-            name: json.LocalizedName
-          }
+            name: json.LocalizedName,
+          },
         });
       });
   };
@@ -29,46 +29,47 @@ export const getLocation = () => dispatch => {
 export const getCurForecast = cityKey => dispatch => {
   let key = cityKey;
   fetch(
-    `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=fB40fnXIHKyL5EH90VyfZ8YbIQeHtv61`
+    `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=fB40fnXIHKyL5EH90VyfZ8YbIQeHtv61`
   )
     .then(response => response.json())
     .then(json => {
       let info = json.DailyForecasts;
       dispatch({
         type: GET_CURRENT_CITY_FORECAST,
-        payload: info
+        payload: info,
       });
     });
 };
 
 export const changeCity = name => dispatch => {
   fetch(
-    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=fB40fnXIHKyL5EH90VyfZ8YbIQeHtv61&q=${name}`
+    `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=fB40fnXIHKyL5EH90VyfZ8YbIQeHtv61&q=${name}`
   )
     .then(response => response.json())
     .then(json => {
-      console.log(json);
-      dispatch({
-        type: CHANGE_CITY,
-        payload: {
-          key: json[0].Key,
-          name: json[0].LocalizedName
-        }
-      });
+      if (json[0].key && json[0].LocalizedName) {
+        dispatch({
+          type: CHANGE_CITY,
+          payload: {
+            key: json[0].Key,
+            name: json[0].LocalizedName,
+          },
+        });
+      }
     });
 };
 
 export const getHourly = key => dispatch => {
   fetch(
-    `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${key}?apikey=fB40fnXIHKyL5EH90VyfZ8YbIQeHtv61`
+    `https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${key}?apikey=fB40fnXIHKyL5EH90VyfZ8YbIQeHtv61`
   )
     .then(response => response.json())
     .then(json => {
       dispatch({
         type: GET_HOURLY,
         payload: {
-          forecast: json
-        }
+          forecast: json,
+        },
       });
     });
 };
